c4e5e3252d86b02f6b9df1799f31199ed3156a4e
Marc Carré <carre.marc@gmail.com>
2018-11-04 19:12:41 +0900

    Tests now explicitly use environment variables

--- pkg/gtasks/gtasks_test.go
+++ pkg/gtasks/gtasks_test.go
@@ -9,11 +9,18 @@ import (
 )
 
 func TestDeleteAll(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Long running test TestDeleteAll skipped.")
-	}
-	client, err := gtasks.NewClient(credentials.DefaultPath)
+	checkPreconditions(t)
+	client, err := gtasks.NewClientFromEnvVars()
 	assert.NoError(t, err)
 	err = client.DeleteAll()
 	assert.NoError(t, err)
+}
+
+func checkPreconditions(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipped TestDeleteAll: short tests only should be run.")
+	}
+	if !credentials.EnvVarsSet() {
+		t.Skip("Skipped TestDeleteAll: required environment variables are missing.")
+	}
 }
